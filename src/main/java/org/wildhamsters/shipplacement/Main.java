1511e383a2a7e81045da04179eac4ce496b0f1f0
Kamil Koziol <kamil_koziol@epam.com>
2022-05-11 09:06:15 +0200

    added spring boot configuration

--- src/main/java/org/wildhamsters/shipplacement/Main.java
+++
@@ -1,8 +0,0 @@
-package org.wildhamsters.shipplacement;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
-	
---
+++ src/test/java/org/wildhamsters/shipplacement/ShipPlacementTest.java
@@ -0,0 +1,13 @@
+package org.wildhamsters.shipplacement;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.testng.annotations.Test;
+
+@SpringBootTest
+class ShipPlacementTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
