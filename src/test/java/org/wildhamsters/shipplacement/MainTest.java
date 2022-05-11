7cb41f846d55e07a264b2738616526b406ca2608
Kamil Koziol <kamil_koziol@epam.com>
2022-05-11 08:58:31 +0200

    init commit

---
+++ src/test/java/org/wildhamsters/shipplacement/MainTest.java
@@ -0,0 +1,12 @@
+package org.wildhamsters.shipplacement;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class MainTest {
+    @Test
+    public void simpleTest() {
+        Assert.assertTrue(true);
+    }
+}
+
