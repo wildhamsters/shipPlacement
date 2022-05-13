c7a965e7c6ef3f52aae149d79ee051fdb73e6f9d
d.żebracki <d.zebracki@gmail.com>
2022-05-13 09:38:59 +0200

    Add data provider to tests

--- src/test/java/org/wildhamsters/shipplacement/configuration/ShipPlacementConfigurerTest.java
+++ src/test/java/org/wildhamsters/shipplacement/configuration/ShipPlacementConfigurerTest.java
@@ -1,9 +1,11 @@
 package org.wildhamsters.shipplacement.configuration;
 
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Random;
 import java.util.stream.IntStream;
 
@@ -13,16 +15,29 @@ public class ShipPlacementConfigurerTest {
     private final static int DISTINCT_PLACEMENT_TRESHOLD = 20;
     private final static Random RANDOM = new Random();
 
-    public void shouldReturnDifferentShipPositions_whenInvokedMultipleTimes() {
+    @Test(dataProvider = "configurationInput")
+    public void shouldReturnDifferentShipPositions_whenInvokedMultipleTimes(int height, int width, List<Integer> shipsToBePlaced) {
         //given
-        var configurer = new ShipPlacementConfigurer(10, 10, RANDOM);
+        var configurer = new ShipPlacementConfigurer(height, width, RANDOM);
         //when
         var distinctPlacements = IntStream.range(0, 20).asDoubleStream()
                 .boxed()
-                .map(i -> configurer.placeShips(List.of(4, 3, 3, 2, 2, 2, 1, 1, 1, 1)))
+                .map(i -> configurer.placeShips(shipsToBePlaced))
                 .distinct()
                 .count();
         //then
         Assert.assertNotEquals(distinctPlacements, DISTINCT_PLACEMENT_TRESHOLD);
+    }
+
+    @DataProvider
+    Object[][] configurationInput() {
+        return new Object[][] {
+                {10, 10, List.of(4, 3, 3, 2, 2, 2, 1, 1, 1, 1)},
+                {10, 10, List.of(3, 3, 2, 2, 2, 1, 1, 1)},
+                {10, 10, List.of(3, 2, 2, 2, 1, 1, 1)},
+                {10, 10, List.of(4, 3, 2, 2, 2, 1, 1, 1)},
+                {10, 10, List.of(4, 3, 3, 2, 2, 2, 1, 1, 1, 1)},
+                {10, 10, List.of(4, 3, 3)}
+        };
     }
 }
